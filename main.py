861ae7c17d2cbe72c658115b2c5a6ef722c9f4bf
Xeli0701 <yamato232@live.jp>
2021-03-13 22:16:12 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 8080
+
+from fastapi import FastAPI
+from janome.tokenizer import Tokenizer
+import oseti
+
+app = FastAPI()
+
+@app.get("/nyan/{message}")
+async def getNyanMessage(message):
+    t = Tokenizer()
+    message_tokens = []
+    for token in t.tokenize(message):
+        if("終助詞" in token.part_of_speech):
+            pass
+        else:
+            message_tokens.append(token.surface)
+
+    converted_message = "".join(message_tokens) + "にゃ"
+    return {"message": converted_message}
+
+
+@app.get("/posneg/{message}")
+async def getPosNegResponse(message):
+    analyzer = oseti.Analyzer()
+    temp = analyzer.analyze(message)
+    score = sum(temp) / len(temp)
+    print(score)
+    
+    if (score >= 0.5):
+        return {"message": "すばらしいにゃ！"}
+    elif (score > -0.5):
+        return {"message": "なるほどにゃ"}
+    elif (score <= -0.5):
+        return {"message": "そうなのかにゃ……"}
+    else:
+        return {"message": "にゃーん"}
