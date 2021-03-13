c753ed61f4b4f535f10e81ff9f3326fc36f29b98
Xeli0701 <yamato232@live.jp>
2021-03-14 03:35:17 +0900

    TODO na to Nya

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 @app.get("/nyan/{message}")
 async def getNyanMessage(message):
+    #「な」を「にゃ」に変換する
     t = Tokenizer()
     message_tokens = []
     for token in t.tokenize(message):
@@ -20,19 +21,19 @@ async def getNyanMessage(message):
     return {"message": converted_message}
 
 
-@app.get("/posneg/{message2}")
-async def getPosNegResponse(message2):
-    message = urllib.parse.unquote(message2)
+@app.get("/posneg/{message}")
+async def getPosNegResponse(message):
+    message = urllib.parse.unquote(message)
     analyzer = oseti.Analyzer()
     temp = analyzer.analyze(message)
     score = sum(temp) / len(temp)
     print(score)
     
-    if (score >= 0.5):
+    if (score >= 0.5): #「喜」とかのポジティブ
         return {"message": "すばらしいにゃ！"}
-    elif (score > -0.5):
+    elif (score > -0.5): #ふつう
         return {"message": "なるほどにゃ"}
-    elif (score <= -0.5):
+    elif (score <= -0.5): #「死」とかのネガティブ
         return {"message": "そうなのかにゃ……"}
-    else:
+    else: #エラー
         return {"message": "にゃーん"}
