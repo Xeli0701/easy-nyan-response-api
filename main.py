767c15d80666f8013ea21bd7ba6b965b008f5e79
Xeli0701 <yamato232@live.jp>
2021-03-14 10:15:54 +0900

    軽量化テスト

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ async def getNyanMessage(message):
         else:
             message_tokens.append(token.surface)
 
-    temp = "".join(message_tokens) + "にゃ"
+    #temp = "".join(message_tokens) + "にゃ"
     converted_message = temp.replace("な","にゃ")
      
     print(converted_message)
     return {"message": converted_message}
 
 
-@app.get("/posneg/{message}")
-async def getPosNegResponse(message):
+@app.get("/posneg/{message2}")
+async def getPosNegResponse(message2):
     #感情分析して、その結果を元に相槌を返す
-    message = urllib.parse.unquote(message)
+    message = urllib.parse.unquote(message2)
     analyzer = oseti.Analyzer()
     temp = analyzer.analyze(message)
     score = sum(temp) / len(temp)
