9293ee763175caa48a509396db7b821dadd444f6
Xeli0701 <yamato232@live.jp>
2021-03-14 10:44:53 +0900

    メモリ利用の修正・コメント追加

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from janome.tokenizer import Tokenizer
-# import oseti
-# import urllib.parse
+import oseti
+import urllib.parse
 
 app = FastAPI()
 
 @app.get("/nyan/{message}")
 async def getNyanMessage(message):
+    #「な」を「にゃ」に変換する・語尾をいい感じに「にゃ」にする
+    message = urllib.parse.unquote(message)
     t = Tokenizer()
     message_tokens = []
     for token in t.tokenize(message):
@@ -22,19 +24,20 @@ async def getNyanMessage(message):
     return {"message": converted_message}
 
 
-# @app.get("/posneg/{message2}")
-# async def getPosNegResponse(message2):
-#     message = urllib.parse.unquote(message2)
-#     analyzer = oseti.Analyzer()
-#     temp = analyzer.analyze(message)
-#     score = sum(temp) / len(temp)
-#     print(score)
+@app.get("/posneg/{message}")
+async def getPosNegResponse(message):
+    #感情分析して、その結果を元に相槌を返す
+    message = urllib.parse.unquote(message)
+    analyzer = oseti.Analyzer()
+    temp = analyzer.analyze(message)
+    score = sum(temp) / len(temp)
+    print(score)
     
-#     if (score >= 0.5): #「喜」とかのポジティブ
-#         return {"message": "すばらしいにゃ！"}
-#     elif (score > -0.5): #ふつう
-#         return {"message": "なるほどにゃ"}
-#     elif (score <= -0.5): #「死」とかのネガティブ
-#         return {"message": "そうなのかにゃ……"}
-#     else: #エラー
-#         return {"message": "にゃーん"}
+    if (score >= 0.5): #「喜」とかのポジティブ
+        return {"message": "すばらしいにゃ！"}
+    elif (score > -0.5): #ふつう
+        return {"message": "なるほどにゃ"}
+    elif (score <= -0.5): #「死」とかのネガティブ
+        return {"message": "そうなのかにゃ……"}
+    else: #エラー
+        return {"message": "にゃーん"}
